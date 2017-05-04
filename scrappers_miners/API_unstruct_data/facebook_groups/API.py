f8b92ec2f43fbed43e4805e7321787325c7513e5
Pranav <pranav.aggarwal06@gmail.com>
2017-05-04 19:20:04 +0530

    partial facebook api structure

--- scrappers_miners/API_unstruct_data/facebook_groups/API.py
+++ scrappers_miners/API_unstruct_data/facebook_groups/API.py
@@ -2,16 +2,15 @@ from urlparse import urlparse
 
 import facebook
 import nltk
+from scrappers_miners.API_unstruct_data.facebook_groups.es_structure import Facebook
 
-ACCESS_TOKEN = 'EAACEdEose0cBAPN0nwT04U4g64Yeelf7fHzbodjzYBkHuuPS9QZBxe2xDuz6LN2oNGClKT5CbAxgeByyM6h0ZClc5eGGm5kx3hh6TYiHJR6kWGJRMeQC4SZA5cRa3EAD2vmZALGcZB9y7dQdAY2ZCkcFID8fJrpKhSqjdnpN11b58UjHHVtuXX7xwSUmsXmv4ZD'
+ACCESS_TOKEN = 'EAACEdEose0cBAOZCerQ9GFutd3cc8DcZABCOwfFgUdN9QjgguPpuVGH86Wifz2Gy1hjNNxlMvSDtJiuPcCiO6jqAeNYXpmbZAvQZBugLbhhmkFVptDPIcMrUaiZAig04HnVXZATh8cjmsRzrJanD9cabrXi2ChiASpREHxSNEBPVfpuBtja0FqRNROatlCNrkZD'
 
 
 # TODO: Exceptions and Activity Log
 
 
 class FacebookGroupCrawler(object):
-    group_detail_list = ['id', 'cover', 'description', 'name']
-
     def __init__(self, access_token, version='2.8'):
         try:
             self.graph = facebook.GraphAPI(access_token=access_token, version=version)
@@ -28,10 +27,17 @@ class FacebookGroupCrawler(object):
             self.cp = nltk.RegexpParser(self.grammar)
             self.keywords = {}
 
+    @staticmethod
+    def get_group_fields():
+        return ','.join(Facebook.group_detail_list)
+
     def get_keywords_for_groups(self, groups):
         self.groups = [self.get_group_id(group_dict) for group_dict in groups if group_dict['active']]
         for group, last_update_time in self.groups:
             self.keywords[group] = {}
+
+            self.keywords[group]['group_details'] = self.get_group_details(group, self.get_group_fields())
+
             try:
                 # TODO: Get facebook post link
                 posts = self.graph.get_all_connections(id=group, connection_name='feed',
@@ -46,7 +52,8 @@ class FacebookGroupCrawler(object):
 
                     self.keywords[group][post_id] = {
                         'updated_time': post_update_time,
-                        'keywords': []
+                        'keywords': [],
+                        'group_name': self.keywords[group]['group_details']['name']
                     }
 
                     if post_message:
@@ -82,13 +89,12 @@ class FacebookGroupCrawler(object):
         else:
             # Now we know that t.node is defined
             if t.label() == 'NP':
-                self.keywords[group][post_id]['keywords'].append([leave for leave, typ in t.leaves()])
+                self.keywords[group][post_id]['keywords'].append(''.join([leave for leave, typ in t.leaves()]))
             for child in t:
                 self.traverse(child, group, post_id)
 
-    @classmethod
-    def get_group_detail_list(cls):
-        return cls.group_detail_list
+    def get_group_details(self, group_id, fields):
+        return self.graph.get_object(id=group_id, fields=fields)
 
 
 def main_method(*args, **kwargs):
@@ -110,3 +116,16 @@ def main_method(*args, **kwargs):
 
     fb = FacebookGroupCrawler(ACCESS_TOKEN)
     return fb.get_keywords_for_groups(ex_data['data'])
+
+
+abc = main_method(ex_details={
+    'data': [
+        {
+            'link': 'https://www.facebook.com/groups/115194395496242/',
+            'update_time': None,
+            'active': True
+        }
+    ]
+})
+
+print abc
