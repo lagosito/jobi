c20e0c7eb72fb3578d28cb4095b081e93e280cdf
Pranav <pranav.aggarwal06@gmail.com>
2017-05-04 21:50:35 +0530

    test search api

--- user_custom/urls.py
+++ user_custom/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from user_custom.views import main_view
+from user_custom.views import main_view, search_view
 
 urlpatterns = [
     url(r'^$', main_view, name='main'),
+    url(r'^search/$', search_view, name='search_view'),
+
 ]
--- user_custom/views.py
+++ user_custom/views.py
@@ -1,7 +1,25 @@
+# FIXME
+from django.http.response import JsonResponse
+
 from django.shortcuts import render
 
 # Create your views here.
+from elasticsearch.client import Elasticsearch
 
 
 def main_view(request):
+
     return render(request, 'main.html', {})
+
+
+def search_view(request):
+    print request.GET
+    if request.GET.get('keyword'):
+        keyword = request.GET.get('keyword')
+
+        es = Elasticsearch()
+        res = es.search(index="jobi", body={"query": {"match": {"_all": str(keyword)}}})
+
+        return JsonResponse(res['hits'])
+    else:
+        return JsonResponse({"status": "KAT GYA"})
