e6388c652dfb3538f012c9c922fac5265ed20775
Pranav <pranav.aggarwal06@gmail.com>
2017-05-08 04:22:26 +0530

    search with filters, recent jobs and newsletter subscription api added

--- essentials/models.py
+++ essentials/models.py
@@ -1,4 +1,10 @@
 from __future__ import unicode_literals
-
+from django.contrib.postgres.fields.jsonb import JSONField
+from django.db import models
 # Create your models here.
 
+
+class NewsletterSubscription(models.Model):
+    email = models.EmailField(unique=True)
+    keywords = JSONField()
+    create_time = models.DateTimeField(auto_now_add=True)
--- user_custom/urls.py
+++ user_custom/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 
-from user_custom.views import main_view, search_view
+from user_custom.views import main_view, search_view, recent_jobs_api, search_job_api, NewsletterSubscriptionView
 
 urlpatterns = [
     url(r'^$', main_view, name='main'),
     url(r'^search/$', search_view, name='search_view'),
-
+    url(r'^recent_jobs/$', recent_jobs_api, name='recent_jobs_api'),
+    
+    url(r'^search_jobs/$', search_job_api, name='search_job_api'),
+    url(r'^newsletter_add_subscription/$', NewsletterSubscriptionView.as_view(), name='newsletter_add_subscription'),
 ]
