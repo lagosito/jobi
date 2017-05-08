479e34ce58b6623bdc7668acf837674701b43765
Rushil <rushil0195@gmail.com>
2017-05-08 22:25:56 +0530

    Mining functions P1 [incomplete]

--- data/utils.py
+++ data/utils.py
@@ -1,5 +1,8 @@
 import abc
 
+from polyglot.text import Text
+from polyglot.mapping import Embedding
+
 
 class APIHead(object):
     def __init__(self, *args, **kwargs):
@@ -21,3 +24,50 @@ class APIHead(object):
             return self.data_iterator
         else:
             raise NotImplementedError("data_iterator not assigned an iterable object.")
+
+
+class NLP(object):
+    def __init__(self, sen, *args, **kwargs):
+        self.sen = sen
+        self.text = Text(self.sen)
+        pass
+
+    def validate(self):
+        self.sen = Text
+        return True
+
+
+def get_NER():
+    embeddings = Embedding.load("/home/rushil/polyglot_data/embeddings2/en/embeddings_pkl.tar.bz2")
+    li = ['jobs', 'employee', 'freelance', 'employment', 'fulltime', 'work', 'parttime', 'placement', 'gig',
+          'incumbency', 'occupation', 'opportunity', 'work', 'development']
+    neighbors = []
+
+    for word in li:
+        try:
+            neighbors += embeddings.nearest_neighbors(word, top_k=10)
+        except:
+            pass
+
+    for n in set(neighbors + li):
+        print n
+        # for n in li:
+        #     print n
+
+
+# tokens = [token for token in tokens if token not in stopwords]
+
+def get_NER2():
+    sen = "Backend Developer Needed. Location: Greater Seattle Area Company: Soda.com Description: We 2019 are looking for a Web Developer with an emphasis on Wordpress to join our development team as we increase our capacity in 2017. PHP, MySQL, HTML, CSS, JS, Git, Precompilers, Gulp."
+    text = Text(sen)
+    print(text.pos_tags)
+
+
+def get_NER3():
+    sen = "Backend Developer Needed. Location: Greater Seattle Area Company: Soda.com Description: We 2019 are looking for a Web Developer with an emphasis on Wordpress to join our development team as we increase our capacity in 2017. PHP, MySQL, HTML, CSS, JS, Git, Precompilers, Gulp."
+    # rake_object = RAKE.Rake('/stoplists/SmartStoplist.txt', 5, 3, 4)
+
+
+# def get_NER()
+# def filter_sen()
+get_NER()
